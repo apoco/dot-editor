531b5a5c0fc9e2960516449360fc93c36a34517a
Jacob Page <jacob.h.page@gmail.com>
2018-07-21 17:40:35 -0700

    Editor resizing

--- src/ui/components/app.js
+++ src/ui/components/app.js
@@ -3,8 +3,10 @@ const { ipcRenderer } = require("electron");
 
 import { RENDER_RESULT, SOURCE_CHANGED } from "../../constants/messages";
 import Editor from "./editor";
+import prefs from "../../prefs";
 
 const lineNumRegex = /\bline (\d+)/;
+const EDITOR_WIDTH_PREF = "layouts.horizontal.editorWidth";
 
 class AppComponent extends React.Component {
   constructor() {
@@ -13,10 +15,14 @@ class AppComponent extends React.Component {
     this.state = {
       code: "",
       svg: "",
-      errors: ""
+      errors: "",
+      editorWidth: prefs.get(EDITOR_WIDTH_PREF, 500),
+      resizeDelta: 0
     };
 
-    this.handleChange = this.handleChange.bind(this);
+    this.isResizing = false;
+    this.resizePointer = null;
+    this.resizeStart = null;
   }
 
   componentDidMount() {
@@ -25,10 +31,41 @@ class AppComponent extends React.Component {
     });
   }
 
-  handleChange(code) {
+  handleChange = code => {
     ipcRenderer.send(SOURCE_CHANGED, code);
     this.setState({ code });
-  }
+  };
+
+  handleSplitterGrab = e => {
+    this.isResizing = true;
+    this.resizeStart = e.screenX;
+    this.resizePointer = e.pointerId;
+    e.currentTarget.setPointerCapture(e.pointerId);
+  };
+
+  handleSplitterMove = e => {
+    if (!this.isResizing) {
+      return;
+    }
+
+    this.setState({
+      resizeDelta: e.screenX - this.resizeStart
+    });
+  };
+
+  handleSplitterRelease = e => {
+    e.currentTarget.releasePointerCapture(this.resizePointer);
+
+    this.isResizing = false;
+    const newWidth = this.state.editorWidth + e.screenX - this.resizeStart;
+
+    this.setState({
+      editorWidth: newWidth,
+      resizeDelta: 0
+    });
+
+    prefs.set(EDITOR_WIDTH_PREF, newWidth);
+  };
 
   parseErrors() {
     const { errors } = this.state;
@@ -41,20 +78,30 @@ class AppComponent extends React.Component {
   }
 
   render() {
-    const { code, svg, errors } = this.state;
+    const { code, svg, editorWidth, resizeDelta } = this.state;
 
     return (
-      <React.Fragment>
+      <div
+        id="app"
+        style={{
+          gridTemplateColumns: `${editorWidth + resizeDelta}px 1px auto`
+        }}
+      >
         <Editor
           value={code}
           annotations={this.parseErrors()}
           onChange={this.handleChange}
         />
-        <div id="gripper">
-          <div id="gripper-handle"/>
+        <div
+          id="gripper"
+          onPointerDown={this.handleSplitterGrab}
+          onPointerMove={this.handleSplitterMove}
+          onPointerUp={this.handleSplitterRelease}
+        >
+          <div id="gripper-handle" />
         </div>
         <div id="diagram" dangerouslySetInnerHTML={{ __html: svg }} />
-      </React.Fragment>
+      </div>
     );
   }
 }
