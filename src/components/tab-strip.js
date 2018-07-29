d09e1d1310f2411bdb9564664e50a0ec595ee30a
Jacob Page <jacob.h.page@gmail.com>
2018-07-29 10:05:26 -0700

    Nicer-looking tabs

--- src/components/tab-strip.js
+++ src/components/tab-strip.js
@@ -11,18 +11,17 @@ class TabStrip extends React.Component {
 
     return (
       <div id="tab-strip">
-        <div id="tab-strip-inner">
-          {tabs.map(({ tabId, filename, isDirty }) => (
-            <div
-              className={classNames("tab", { active: tabId === activeTabId })}
-              key={tabId}
-              onClick={onTabSelected.bind(null, tabId)}
-            >
-              {filename || "<untitled>"}
-              {isDirty && " *"}
-            </div>
-          ))}
-        </div>
+        {tabs.map(({ tabId, filename, isDirty }) => (
+          <div
+            className={classNames("tab", { active: tabId === activeTabId })}
+            key={tabId}
+            onClick={onTabSelected.bind(null, tabId)}
+          >
+            {filename || "<untitled>"}
+            {isDirty && " *"}
+          </div>
+        ))}
+        <div id="tab-strip-border"></div>
       </div>
     );
   }
